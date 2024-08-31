bd612cbe4d092708bd31b7f492571e91665c7fbb
starobba <116376529+starobba@users.noreply.github.com>
2024-09-01 01:57:11 +0900

    Add project files.

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// 변수선언
+string name = string .Empty; //초기화 해주려고 쓴거 <-- ""이거 대신에 쓴거임
+int age = 0;
+int retirementAge = 65;
+
+// 유저프롬프트
+Console.WriteLine("이름을 입력하세요");
+name = Console.ReadLine();
+
+Console.WriteLine("나이를 입력하세요");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process the data
+int workingYearsRemaining = retirementAge - age;
+
+// output
+Console.WriteLine($"이름: {name}");
+Console.WriteLine($"나이: {age}");
+Console.WriteLine($"퇴사까지 남은시간: {workingYearsRemaining}");
+
+Console.ReadKey();
---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// decimal - double, float, decimal
+
+string name = "Youngwoo";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // string concatenation
+Console.WriteLine($"They call me {name}"); // string interpolation
+Console.WriteLine("I was given the name {0}", name); // Formatted string
+
